79a4c80c13b814b9fa8f462af02a272edbb11a53
Fang Ming <fangming19872006@gmail.com>
2021-07-31 10:54:12 +0800

    Put source file into src folder.

--- index.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const app = express();
-const PORT = 8000;
-app.get('/', (req,res) => res.send('Express + TypeScript Server js'));
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
-});
