6ec260d990040207d0141cb477d0f7b1cec666a7
Fang Ming <fangming19872006@gmail.com>
2021-07-31 10:48:06 +0800

    Add build, start, dev script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const PORT = 8000;
-app.get('/', (req,res) => res.send('Express + TypeScript Server'));
+app.get('/', (req,res) => res.send('Express + TypeScript Server js'));
 app.listen(PORT, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
 });
