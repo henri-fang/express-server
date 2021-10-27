7dc724d9821333257b75eb4965ad23d8fc540a83
Fang.Ming <fang.ming@myob.com>
2021-10-27 18:47:15 +0800

    Implement the Joi validation.

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import Joi from 'joi'
 
 const router = express.Router()
 const users = [
@@ -6,10 +7,23 @@ const users = [
     {id: 2, name: " ming 2"},
     {id: 3, name: " ming 3"}
 ];
+const schema = Joi.object({
+    id: Joi.number().min(1).message('id must be number'),
+    name: Joi.string().min(1).max(32).message('name is invalid.')
+});
+
+const validateFn = (requestEntity: {}, entitySchema: Joi.ObjectSchema): Joi.ValidationResult =>
+    entitySchema.validate(requestEntity)
+
 router.get('/:id', (req, res) => {
+    const {error} = validateFn(req.params, schema);
+    if (error) {
+        res.status(400).send(error.details[0].message);
+        return;
+    }
     const user = users.find(u => u.id === parseInt(req.params.id));
-    if(!user) res.status(404).send(`user id ${req.params.id} not found.`)
-    res.send(user)
+    if (!user) res.status(404).send(`user id ${req.params.id} not found.`)
+    res.send(user);
 })
 
 router.post('/', (req, res) => {
