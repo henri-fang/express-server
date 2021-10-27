af390ea76d8491a20bc9caca2aa776b6a9ea6a64
Fang.Ming <fang.ming@myob.com>
2021-10-27 10:58:04 +0800

    Enable evn PORT for the server port.

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import express from 'express';
 import { router as userRouter } from './router/userRouter';
 
 const app = express();
-const PORT = 8000;
+const port = process.env.PORT || 8000;
 app.get('/', (_, res) => res.send('Express + TypeScript Server'));
 app.use('/user', userRouter);
 
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
