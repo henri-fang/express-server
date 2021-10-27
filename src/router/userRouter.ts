d77d63e9b0cf0ffa2d14a4b392c45fb0206884b2
Fang.Ming <fang.ming@myob.com>
2021-10-27 10:58:49 +0800

    Implement 404 response on getting user by id.

--- src/router/userRouter.ts
+++ src/router/userRouter.ts
@@ -1,13 +1,19 @@
 import express from 'express'
 
 const router = express.Router()
-
-router.get('/', (req, res) => {
-    res.send('Get user')
+const users = [
+    {id: 1, name: " ming 1"},
+    {id: 2, name: " ming 2"},
+    {id: 3, name: " ming 3"}
+];
+router.get('/:id', (req, res) => {
+    const user = users.find(u => u.id === parseInt(req.params.id));
+    if(!user) res.status(404).send(`user id ${req.params.id} not found.`)
+    res.send(user)
 })
 
 router.post('/', (req, res) => {
     res.send('Create user')
 })
 
-export { router };
+export {router};
